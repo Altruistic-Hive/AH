91741cbd909f45969b5fbae650b0408c5065266e
99jik <99junginkim@gmail.com>
2025-06-15 07:17:37 +0900

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   organizationName: 'Altruistic-Hive', // Usually your GitHub org/user name.
   projectName: 'AH', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
